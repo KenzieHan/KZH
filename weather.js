cf35808b134deac80a3e8d8e6e351d04b68e3062
KenzieHan <117430150+KenzieHan@users.noreply.github.com>
2022-11-07 01:37:25 +0900

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+const API_KEY = "c36ad735fc7ac66ef6198ced0b15a705";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}˚ / `;
+    });
+}
+function onGeoError() {
+  alert("날씨 정보를 허용합니다.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
